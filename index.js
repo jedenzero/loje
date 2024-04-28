ff2770af5e1581f6856ced571c5d679107245d50
1010 <134185805+jedenzero@users.noreply.github.com>
2024-04-28 14:55:41 +0900

    Update index.js

--- index.js
+++ index.js
@@ -142,13 +142,17 @@ function solve(){
     if(limit[type]===0){
       type=(type+1)%3;
     }
-    var index=Math.floor(Math.random()*(limit[type]+1));
-    while(!noSeen[type].includes(toSolve[type][index])&&noSeen[type].length>0){
-      index=Math.floor(Math.random()*(limit[type]+1));
-      console.log(`시도 : ${index}`);
+    if(noSeen[type].length>0){
+      do{
+        index=Math.floor(Math.random()*limit[type]);
+      }
+      while(!noSeen[type].includes(toSolve[type][index]));
+      noSeen[type]=[...noSeen[type].slice(0, noSeen[type].indexOf(toSolve[type][index])), ...noSeen[type].slice(noSeen[type].indexOf(toSolve[type][index])+1)];
+    }
+    else{
+      index=Math.floor(Math.random()*limit[type]);
     }
     var i=toSolve[type][index];
-    noSeen[type]=[...noSeen[type].slice(0,noSeen[type].indexOf(i)),...noSeen[type].slice(noSeen[type].indexOf(i)+1)];
     var j='';
     var n=0;
     var rev=Math.floor(Math.random()*2);
